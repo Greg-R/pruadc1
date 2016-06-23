54cb582dbfb118a62eeb09fb8ab3a8b8012bcea9
Greg Raven <soapy-smith@comcast.net>
2016-06-23 22:35:34 +0000

    fixed buffer problem, now uint16_t works

--- rpmsg_data_read.c
+++ rpmsg_data_read.c
@@ -1,34 +1,5 @@
 /*
  * Copyright (C) 2015 Texas Instruments Incorporated - http://www.ti.com/
- *
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *	* Redistributions of source code must retain the above copyright
- *	  notice, this list of conditions and the following disclaimer.
- *
- *	* Redistributions in binary form must reproduce the above copyright
- *	  notice, this list of conditions and the following disclaimer in the
- *	  documentation and/or other materials provided with the
- *	  distribution.
- *
- *	* Neither the name of Texas Instruments Incorporated nor the names of
- *	  its contributors may be used to endorse or promote products derived
- *	  from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include <stdio.h>
@@ -51,6 +22,7 @@ int main(void)
 	struct pollfd pollfds[1];
 	int i;
 	int result = 0;
+        uint16_t sendPayload[1] = {29};
 
 	/* Open the rpmsg_pru character device file */
 	pollfds[0].fd = open(DEVICE_NAME, O_RDWR);
@@ -72,7 +44,7 @@ int main(void)
 	for (i = 0; i < NUM_MESSAGES; i++) {
 		/* Send 'hello world!' to the PRU through the RPMsg channel */
 	//	result = write(pollfds[0].fd, "hello world!", 13);
-		result = write(pollfds[0].fd, 21, 1);
+		result = write(pollfds[0].fd, sendPayload, 2);
 		if (result > 0)
 			printf("Message %d: Sent to PRU\n", i);
 
