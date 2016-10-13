6251b34518db7588b05c3a41399f49128eae268b
Greg <soapy-smith@comcast.net>
2016-10-13 18:26:21 -0400

    Major cleanup.

--- pru0adc.c
+++ pru0adc.c
@@ -6,6 +6,20 @@
 //  MISO:         P9.28 pr1_pru0_pru_r31_3
 //  SPI CLK:      P9.30 pr1_pru0_pru_r30_2
 //  Sample Clock: P8.46 pr1_pru1_pru_r30_1  (testing only)
+//  Copyright (C) 2016  Gregory Raven
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <stdint.h>
 #include <stdio.h>
